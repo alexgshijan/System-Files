ac656c7c36fd9524dcc080932b68e0e4c0b79243
BlancOfficial <alexgshijan@gmail.com>
2024-11-25 14:14:54 +0000

    session management fix

--- auth/sessionManagement.js
+++
@@ -1,5 +0,0 @@
-function validateAccountType(accountType) {
-    if (sessionStorage.getItem('account_type') !== accountType) {
-        window.location.href = 'https://solarportal.alexgs.co.uk';
-    }
-}
